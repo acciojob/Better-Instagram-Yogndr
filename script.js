131bbc3db23c0c8aa5f30b75216e078cee92d8c6
Yogndr <106022206+Yogndr@users.noreply.github.com>
2024-12-30 00:48:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+document.getElementsByClassName("image").addEventListener("drag",(e)=>{
+	
+});
+document.getElementsByClassName("image").addEventListener("ondragover",(e)=>{
+	e.preventdefault()
+});
+document.getElementsByClassName("image").addEventListener("ondrop",(e)=>{
+	
+});
